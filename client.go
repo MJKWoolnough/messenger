2ef005201fefe5b3a84e29f46cad4e10662f886a
Michael Woolnough <michael.woolnough@gmail.com>
2018-04-18 16:01:28 +0100

    gathered required vars for api calls

--- client.go
+++ client.go
@@ -6,13 +6,17 @@ import (
 	"net/http/cookiejar"
 	"net/url"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/MJKWoolnough/errors"
+	"github.com/MJKWoolnough/memio"
 	"github.com/robertkrimen/otto"
 	"golang.org/x/net/publicsuffix"
 	xmlpath "gopkg.in/xmlpath.v2"
 )
+
+const CLIENT_VERSION = 3822019
 
 const (
 	cDomain   = "https://www.messenger.com/"
@@ -20,7 +24,10 @@ const (
 	cAPIURL   = cDomain + "api/graphql"
 )
 
-var domain, loginURL, apiURL *url.URL
+var (
+	domain, loginURL, apiURL *url.URL
+	pageScripts              = xmlpath.MustCompile("//script[not(@src)]")
+)
 
 func init() {
 	domain, _ = url.Parse(cDomain)
@@ -30,8 +37,11 @@ func init() {
 
 type Client struct {
 	http.Client
-	request uint64
-	userID  uint64
+	postData                url.Values
+	username, usernameShort string
+
+	requestMu sync.Mutex
+	request   uint64
 }
 
 func NewClient(cookies []*http.Cookie) *Client {
@@ -53,7 +63,7 @@ func (c *Client) Resume() error {
 	if resp.StatusCode != 302 {
 		return ErrInvalidCookies
 	}
-	return nil
+	return c.init()
 }
 
 func (c *Client) Login(username, password string) error {
@@ -70,17 +80,20 @@ func (c *Client) Login(username, password string) error {
 		cookieSet   bool
 		cookieValue string
 	)
-	vm := otto.New()
-	vm.Set("setValue", func(call otto.FunctionCall) otto.Value {
-		cookieSet = true
-		cookieValue = call.Argument(0).String()
-		return otto.UndefinedValue()
-	})
-	for scripts := xmlpath.MustCompile("//body/script").Iter(nodes); scripts.Next(); {
-		vm.Run(scripts.Node().String())
+	if err = runCode(
+		jsFuncs{
+			"setCookieValue": func(call otto.FunctionCall) otto.Value {
+				cookieSet = true
+				cookieValue = call.Argument(0).String()
+				return otto.UndefinedValue()
+			},
+		},
+		pageScripts.Iter(nodes),
+	); err != nil {
+		return errors.WithContext("error grabbing datr cookie: ", err)
 	}
 	if !cookieSet {
-		return errors.Error("error grabbing datr cookie")
+		return ErrDatrCookie
 
 	}
 	c.Jar.SetCookies(domain, []*http.Cookie{
@@ -126,7 +139,7 @@ func (c *Client) Login(username, password string) error {
 	var goodCookies bool
 	for _, cookie := range c.GetCookies() {
 		if cookie.Name == "c_user" {
-			c.userID, err = strconv.ParseUint(cookie.Value, 10, 64)
+			_, err = strconv.ParseUint(cookie.Value, 10, 64)
 			if err != nil {
 				return errors.WithContext("error parsing userID:", err)
 			}
@@ -139,7 +152,7 @@ func (c *Client) Login(username, password string) error {
 		return ErrInvalidLogin
 	}
 
-	return nil
+	return c.init()
 }
 
 func (c *Client) GetCookies() []*http.Cookie {
@@ -150,7 +163,104 @@ func noRedirect(_ *http.Request, _ []*http.Request) error {
 	return http.ErrUseLastResponse
 }
 
+func (c *Client) init() error {
+	resp, err := c.Get(cDomain)
+	if err != nil {
+		return errors.WithContext("error grabbing init page: ", err)
+	}
+	nodes, err := xmlpath.ParseHTML(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return errors.WithContext("error parsing init page: ", err)
+	}
+
+	var (
+		udSet, dtsgSet, sdSet, snSet bool
+		sprinkleName, sprinkleValue  string
+	)
+
+	c.postData = make(url.Values)
+	c.postData.Set("__a", "1")
+	c.postData.Set("__rev", strconv.FormatUint(CLIENT_VERSION, 10))
+
+	if err = runCode(
+		jsFuncs{
+			"setUserData": func(call otto.FunctionCall) otto.Value {
+				c.postData.Set("__user", call.Argument(0).String())
+				c.username = call.Argument(1).String()
+				c.usernameShort = call.Argument(2).String()
+				udSet = true
+				return otto.UndefinedValue()
+			},
+			"setDTSGToken": func(call otto.FunctionCall) otto.Value {
+				sprinkleValue = call.Argument(0).String()
+				c.postData.Set("fb_dtsg", sprinkleValue)
+				dtsgSet = true
+				return otto.UndefinedValue()
+			},
+			"setSiteData": func(call otto.FunctionCall) otto.Value {
+				c.postData.Set(call.Argument(1).String(), call.Argument(2).String())
+				c.postData.Set(call.Argument(3).String(), call.Argument(4).String())
+				sdSet = true
+				return otto.UndefinedValue()
+			},
+			"setSprinkleName": func(call otto.FunctionCall) otto.Value {
+				sprinkleName = call.Argument(0).String()
+				snSet = true
+				return otto.UndefinedValue()
+			},
+		},
+		pageScripts.Iter(nodes),
+	); err != nil {
+		return errors.WithContext("error getting init values: ", err)
+	}
+
+	if !udSet {
+		err = ErrUnsetUserData
+	} else if !dtsgSet {
+		err = ErrUnsetDTSGToken
+	} else if !sdSet {
+		err = ErrUnsetSiteData
+	} else if !snSet || sprinkleName == "" {
+		err = ErrUnsetSprinkleName
+	}
+	if err != nil {
+		return errors.WithContext("error getting init config: ", err)
+	}
+
+	buf := make(memio.Buffer, 1, 3*len(sprinkleValue)+1)
+	buf[0] = '2'
+	for _, char := range sprinkleValue {
+		fmt.Fprint(&buf, char)
+	}
+
+	c.postData.Set(sprinkleName, string(buf))
+
+	return nil
+}
+
+func (c *Client) PostForm(url string, data url.Values) (*http.Response, error) {
+	for key := range c.postData {
+		data.Set(key, c.postData.Get(key))
+	}
+	c.requestMu.Lock()
+	c.request++
+	req := c.request
+	c.requestMu.Unlock()
+	data.Set("__req", strconv.FormatUint(req, 36))
+	return c.Client.PostForm(url, data)
+}
+
+func (c *Client) GetList() error {
+	return nil
+}
+
 const (
-	ErrInvalidCookies errors.Error = "invalid cookies"
-	ErrInvalidLogin   errors.Error = "invalid login credentials"
+	ErrDatrCookie        errors.Error = "error grabbing datr cookie"
+	ErrInvalidCookies    errors.Error = "invalid cookies"
+	ErrInvalidLogin      errors.Error = "invalid login credentials"
+	ErrUnsetUserData     errors.Error = "user data not set"
+	ErrUnsetDTSGToken    errors.Error = "DTSG token not set"
+	ErrUnsetSiteData     errors.Error = "site data not set"
+	ErrUnsetSprinkleName errors.Error = "sprinkle name not set"
 )
